bd16cf859349e0e2d49d975223785e8c64050a3b
omersaritoy <fltepior@gmail.com>
2024-04-12 01:09:54 +0300

    Add project files.

---
+++ Business/Abstract/IBrandService.cs
@@ -0,0 +1,19 @@
+﻿using Business.Dtos.Responses;
+using Business.Dtos.Requests;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract;
+
+public interface IBrandService
+{
+    //response and Requests
+    CreatedBrandResponse Add(CreateBrandRequest createBrandRequest);
+    List<GetAllBrandResponse> GetAll();
+
+}
+
---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstract;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class BrandManager : IBrandService
+{
+    IBrandDal _brandDal;
+
+    public BrandManager(IBrandDal brandDal)
+    {
+        _brandDal = brandDal;
+    }
+
+    public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+    {
+        //businees rules
+
+        //mapping-->automapper
+        Brand brand = new();
+        brand.Name = createBrandRequest.Name;
+        brand.CreatedDate = DateTime.Now;
+
+        _brandDal.Add(brand);
+
+        CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+        createdBrandResponse.Name = createBrandRequest.Name;
+        createdBrandResponse.Id = 4;
+        createdBrandResponse.CreatedDate = brand.CreatedDate;
+        return createdBrandResponse;
+    }
+
+    public List<GetAllBrandResponse> GetAll()
+    {
+        List<Brand> brands = _brandDal.GetAll();
+        List<GetAllBrandResponse> getAllBrandResponses = new();
+
+        foreach (var brand in brands)
+        {
+            GetAllBrandResponse getAllBrandResponse = new GetAllBrandResponse();
+            getAllBrandResponse.Name = brand.Name;
+            getAllBrandResponse.Id = brand.Id;
+            getAllBrandResponse.CreatedDate = brand.CreatedDate;
+
+            getAllBrandResponses.Add(getAllBrandResponse);
+        }
+        return getAllBrandResponses;
+
+    }
+}
